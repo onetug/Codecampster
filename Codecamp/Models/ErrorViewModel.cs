5bb5c3394df3f8d5e7021c95d564dd316538a494
J Todd Littlejohn <littlejohnjt@hotmail.com>
2018-07-20 10:03:06 -0400

    Add project files.

---
+++ Codecamp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Codecamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
