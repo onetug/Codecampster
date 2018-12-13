1f79c87c68156c8ecfc5b0d500bf4a88d757ff07
Santosh Hari <santosh@NebbiaTech.com>
2018-12-13 16:34:07 -0500

    Added app insights and detailed error message

--- Codecamp/Program.cs
+++ Codecamp/Program.cs
@@ -19,6 +19,7 @@ namespace Codecamp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
