655f92eedbe3164406e8e2a46f2b6377b7618e39
Facundo Gauna <facundo.gauna@newsignature.com>
2019-12-26 15:03:19 -0500

    Refactored the sessionize urls a bit more

--- src/Web/CodeCampster.Web/Pages/Sessions.cshtml.cs
+++ src/Web/CodeCampster.Web/Pages/Sessions.cshtml.cs
@@ -13,5 +13,7 @@ namespace CodeCampster.Web
         {
 
         }
+
+        public string SessionizeUrl => Sessionize.SessionsUrl;
     }
 }
---
+++ src/Web/CodeCampster.Web/Sessionize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeCampster.Web
+{
+    public class Sessionize
+    {
+        public const string ScheduleUrl = "https://sessionize.com/api/v2/klf8046p/view/GridSmart";
+
+        public const string SessionsUrl = "https://sessionize.com/api/v2/klf8046p/view/Sessions";
+
+        public const string SpeakersUrl = "https://sessionize.com/api/v2/klf8046p/view/SpeakerWall";
+    }
+}
