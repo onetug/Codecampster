cd1b09daf1bb2f2ff47cabaaa7ba35f93101c1b3
Facundo Gauna <facundo.gauna@newsignature.com>
2019-12-26 10:20:30 -0500

    Initial commit

---
+++ src/Web/CodeCampster.Web/Pages/Sessions.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeCampster.Web
+{
+    public class SessionsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
