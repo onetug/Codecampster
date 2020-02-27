4a3f3b335898c70803849f46b0680498491d44be
OmnipotentOwl <1769881+OmnipotentOwl@users.noreply.github.com>
2020-02-27 11:22:21 -0500

    Setup logging via Serilog and Correct B2C auth flow issues (#73)
    * Initial work on fixing B2C redirection bug
    
    * Add Azure Front Door infrastucture templates
    
    * Add pipeline template utilities for handling locks and resource group creation
    
    * Update local dev logging level
    
    * Add Views for B2C auth
    
    * cleanup auth flows for B2C to allow redirect uri specification
    
    * cleanup startup unused references
    
    * Update main yaml pipeline to deploy Azure Front Door from ARM Template
    
    * Fix post merge bugs and telemetry.
    
    * Return Login button to page
    
    Co-authored-by: Santosh Hari <desi.american@gmail.com>

---
+++ src/Web/CodeCampster.Web/Areas/AzureADB2C/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeCampster.Web.Auth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CodeCampster.Web.Areas.AzureADB2C.Controllers
+{
+    [NonController]
+    [AllowAnonymous]
+    [Area("AzureADB2C")]
+    [Route("[area]/[controller]/[action]")]
+    internal class AccountController : Controller
+    {
+        private readonly IOptionsMonitor<AzureADB2COptions> _options;
+
+        public AccountController(IOptionsMonitor<AzureADB2COptions> AzureADB2COptions)
+        {
+            _options = AzureADB2COptions;
+        }
+
+        [HttpGet("{scheme?}")]
+        public IActionResult SignIn([FromRoute] string scheme)
+        {
+            scheme = scheme ?? AzureADB2CDefaults.AuthenticationScheme;
+            var redirectUrl = _options.CurrentValue.RedirectUri;
+            return Challenge(
+                new AuthenticationProperties { RedirectUri = redirectUrl },
+                scheme);
+        }
+
+        [HttpGet("{scheme?}")]
+        public IActionResult ResetPassword([FromRoute] string scheme)
+        {
+            scheme = scheme ?? AzureADB2CDefaults.AuthenticationScheme;
+            var options = _options.Get(scheme);
+
+            var redirectUrl = _options.CurrentValue.RedirectUri;
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            properties.Items[AzureADB2CDefaults.PolicyKey] = options.ResetPasswordPolicyId;
+            return Challenge(properties, scheme);
+        }
+
+        [HttpGet("{scheme?}")]
+        public async Task<IActionResult> EditProfile([FromRoute] string scheme)
+        {
+            scheme = scheme ?? AzureADB2CDefaults.AuthenticationScheme;
+            var authenticated = await HttpContext.AuthenticateAsync(scheme);
+            if (!authenticated.Succeeded)
+            {
+                return Challenge(scheme);
+            }
+
+            var options = _options.Get(scheme);
+
+            var redirectUrl = _options.CurrentValue.RedirectUri;
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            properties.Items[AzureADB2CDefaults.PolicyKey] = options.EditProfilePolicyId;
+            return Challenge(properties, scheme);
+        }
+
+        [HttpGet("{scheme?}")]
+        public async Task<IActionResult> SignOut([FromRoute] string scheme)
+        {
+            scheme = scheme ?? AzureADB2CDefaults.AuthenticationScheme;
+            var authenticated = await HttpContext.AuthenticateAsync(scheme);
+            if (!authenticated.Succeeded)
+            {
+                return Challenge(scheme);
+            }
+
+            var options = _options.Get(scheme);
+
+            var callbackUrl = Url.Page("/Account/SignedOut", pageHandler: null, values: null, protocol: Request.Scheme);
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                options.AllSchemes);
+        }
+    }
+}
--- src/Web/CodeCampster.Web/Program.cs
+++ src/Web/CodeCampster.Web/Program.cs
@@ -1,26 +1,76 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CodeCampster.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            Log.Logger = CreateSerilogLogger(configuration);
+            try
+            {
+                Log.Information("Configuring web host ({ApplicationContext})...", AppName);
+                var host = BuildWebHost(configuration, args);
+                Log.Information("Starting web host ({ApplicationContext})...", AppName);
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+        
+        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(false)
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
+                .Build();
+
+        private static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", AppName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
     }
 }
